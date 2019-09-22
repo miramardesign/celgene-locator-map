cd5c66893470c8ddcaaf1e47c5805019da609eeb
Michael Hazzard <miramardesign@gmail.com>
2019-09-22 00:47:21 -0300

    layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MapComponent } from './components/map/map.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    HeaderComponent, 
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    MapComponent
   ],
   imports: [
     SharedModule,
   ],
   exports: [
-    HeaderComponent, 
+    HeaderComponent,
     FooterComponent
   ]
 })
