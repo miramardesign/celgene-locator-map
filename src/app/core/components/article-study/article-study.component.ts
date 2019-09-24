7e80ede9211adb05b9702ace748a7f90f20c1068
Michael Hazzard <miramardesign@gmail.com>
2019-09-24 18:26:17 -0300

    fixed border issues

--- src/app/core/components/article-study/article-study.component.ts
+++ src/app/core/components/article-study/article-study.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ArticleStudyComponent implements OnInit {
 
   @Input() studyClickedList: [];
-
+  @Input() countryClicked: [];
   constructor() { }
 
   ngOnInit() {
