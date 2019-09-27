b77d752d3ae3bd441d81d7395bca682f7a39bf22
Michael Hazzard <miramardesign@gmail.com>
2019-09-27 11:22:36 -0300

    svg changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { version } from '../../package.json';
+// need to add     "resolveJsonModule": true, in tsconfig.json
+export const environment = {
+    VERSION: version,
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Celgene Locator';
+  title = 'Celgene Locator ' + environment.VERSION;
 }
